ef0bd118447274f7bde53f4158c4874732f2f346
Ellis Trickett <ellis.trickett@gmail.com>
2020-11-10 19:10:47 +0000

    vote view added to views and urls

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import Question, Choice
 
@@ -21,4 +24,20 @@ def results(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
   return render(request, 'polls/results.html', { 'question': question })
 
+# Vote for a question choice
+def vote(request, question_id):
+  # print(request.POST['choice'])
+  question = get_object_or_404(Question, pk=question_id)
+  try: 
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    #Redisplay the question voting form.
+    return render(request, 'polls/detail.html', {
+      'question': question,
+      'error_message': "You didnt select a choice.",
+    })
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
